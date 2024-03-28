71f187f12e3bd4ac6c2bee4e11abfe34228ef81e
i.shchepov <i.shchepov@rit.va>
2024-03-28 15:23:01 +0300

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm") version "1.9.23"
+}
+
+group = "com.igorgoose"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework:spring-websocket:6.1.5")
+    implementation("org.springframework:spring-messaging:6.1.5")
+    implementation("org.glassfish.tyrus.bundles:tyrus-standalone-client:2.1.5")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
